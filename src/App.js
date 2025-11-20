cdd3c7e04b5b3ffae48bbc7cb3af67ddc1b0b4d8
SibongileT88 <sibongilet@beskbridge.com>
2025-11-20 17:29:32 +0200

    Removed unnecessary content!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <footer>
+          This project was coded by Sibongile Tsopo. The code is hosted on
+          <a href="" target="_blank" rel="noopener noreferrer">
+            Github
+          </a>
+          . The website is hosted on{" "}
+          <a href="" target="_blank" rel="noopener noreferrer">
+            Netlify
+          </a>
+        </footer>
       </header>
     </div>
   );
